032e0b11a1d858f011484981484d5da3e41f2e2c
jyli <jinyuli@bu.edu>
2021-10-09 00:01:01 -0400

    change the name of dataset:data_emp_data in wranggling_code.R; Add the visualization part in visualization_function.R.

--- visualization_function.R
+++ visualization_function.R
@@ -1,6 +1,21 @@
 head(data_all)
 rlang::last_error()
 
+source("wrangling_code.R")
+
+
+visualize_country <- function(c1, c2){
+  # This function do the comparison betweeen countries about their co2 emission per person and 
+  # the oil consumption in countries per person
+  # I may need to do some try and error to make the program robust
+  # try(if(!data_all[str(c1)] || !data_all[str(c2)]) stop("at least one country data is missing or type error"))
+  
+  a <- try(data_all %>% filter(country == str(c1) || country == str(c2)))
+  if (class(a)[1] == "try-error"){
+    print("at least one country data is missing or type error")
+  }
+  a_co2 <- a %>% select(ends_with("x"))
+}
 
 a <- data_all %>% filter(country == "China")
 a %>% select(ends_with("x")) # select the data end up with "x"
--- wrangling_code.R
+++ wrangling_code.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 path <- getwd()  # get the current working directory
 data_co2_emis <- read_csv(paste0(path, "/co2_emissions_tonnes_per_person.csv"), col_names = T) # read_csv makes the first column as col.name, but col_name can make it 
-data_emp_data <- read_csv(paste0(path, "/oil_consumption_per_cap.csv"), col_names = T)
+data_oil_csmpt <- read_csv(paste0(path, "/oil_consumption_per_cap.csv"), col_names = T)
 
 # data_co2_emis_2 <- data_co2_emis[(nrow(data_co2_emis) - 64 :
                                     # nrow(data_co2_emis))]
@@ -13,8 +13,8 @@ data_co2_emis_2 <- data_co2_emis[(nrow(data_co2_emis) - 27) : 220]
 # add the country column
 data_co2_emis_2 <- add_column(data_co2_emis_2, data_co2_emis["country"])
 
-# combine data_emp_data and data_co2_emis_2
-data_all <- left_join(data_emp_data, data_co2_emis_2, by = "country")
+# combine data_oil_csmpt and data_co2_emis_2
+data_all <- left_join(data_oil_csmpt, data_co2_emis_2, by = "country")
 
 # delete the rows with NA
 data_all <- na.omit(data_all)
